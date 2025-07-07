be6d13967f771a113f524c7ddc27a371d08eafe0
Muhammad Arshman <itxarshman@gmail.com>
2025-07-07 16:16:45 +0500

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -1,19 +1,3 @@
-#  Moon-Userbot - telegram userbot
-#  Copyright (C) 2020-present Moon Userbot Organization
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from sys import version_info
 from .db import db
 import git
@@ -40,7 +24,7 @@ try:
 except git.exc.InvalidGitRepositoryError:
     repo = git.Repo.init()
     origin = repo.create_remote(
-        "origin", "https://github.com/The-MoonTg-project/Moon-Userbot"
+        "origin", "https://github.com/iArshman/open-tg"
     )
     origin.fetch()
     repo.create_head("main", origin.refs.main)
